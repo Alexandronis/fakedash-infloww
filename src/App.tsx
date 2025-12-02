3211a7040d44a75d290501f940794ec2b98b21c3
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-12-02 23:25:03 +0200

    refactoring + current version of header.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="main-container">
+    <div className="dashboard-wrap">
       <Header />
       <Sidebar />
       <div className="main-content">
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -5,8 +5,106 @@ const Header: React.FC = () => {
   //const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="header-wrapper">
-      Header
+    <div className="dashboard-header justify-content-between justify-content-end">
+      <div className="d-flex align-items-center" id="header_left">
+        <div className="sidebar-logo-wrap">
+          <button type="button" className="menu-btn">
+            <img src="/hambuger-icon.png" alt="" />
+          </button>
+        </div>
+      </div>
+      <div className="d-flex align-items-center" id="header_right">
+        <div className="dropdown operational">
+          <p className="referrals-link dropdown-toggle-p">
+            <span />
+            Operational
+          </p>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Operational</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#F88056'}}></span>
+              Partials Outage
+            </a></li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Proxy service</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Messages and notifications</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Growth tools</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Creator and employee management</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Billing and balances</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Share for share</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Affiliate manager</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Infloww.com</a>
+            </li>
+            <li><a className="dropdown-item" href="#">
+              <span style={{backgroundColor: '#58A744'}}></span>
+              Reports</a>
+            </li>
+          </ul>
+        </div>
+        <p className="time-zone"><button type="button" className="tooltip-custom info-icon" data-bs-toggle="tooltip" style="margin-right: 10px; margin-left: 5px">
+          <img src="index_files/top-globe-icon.png" alt="" />
+        </button>UTC+01:00<button type="button" className="tooltip-custom info-icon" data-bs-toggle="tooltip" style="margin-right: 5px; margin-left: 5px" data-bs-placement="top" data-bs-title="Tooltip on top">
+          <img src="index_files/top-info-icon.png" alt="" />
+        </button></p>
+        <a href="#" className="referrals-link">
+          <img src="index_files/referral-icon.png" style="margin-right: 8px" alt="" />
+            <span>Referrals</span>
+        </a>
+        <a href="#" className="referrals-link" style="margin-right: 0">
+          <img src="index_files/trump-icon.png" style="margin-right: 8px" alt="" />
+            <span>Leaderboard</span>
+        </a>
+        <label className="toggle-container">
+          <span className="toggle-label" style="color: rgb(97, 97, 97);">SFW</span>
+          <div className="toggle-switch">
+            <input type="checkbox" id="sfw-toggle" />
+              <div className="slider"></div>
+          </div>
+        </label>
+        <a href="#" style="margin-right: 16px" className="notification-link active"><img src="index_files/alarm-icon.png" alt="" /></a>
+        <div className="profile-dropdown dropdown">
+          <button id="editableText" className="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <span className="avatarName">Ag</span>
+          </button>
+          <ul className="dropdown-menu" style="">
+            <li><a className="dropdown-item" href="#">Add Account</a>
+            </li>
+            <li><a className="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </div>
+
+
+        <a href="#" style="background-color: transparent" className="notification-link window-buttons"><img src="index_files/min-icon.png" alt="" /></a>
+
+        <a href="#" style="background-color: transparent" className="notification-link window-buttons"><img src="index_files/max-icon.png" alt="" /></a>
+
+        <a href="#" style="background-color: transparent" className="notification-link window-buttons"><img src="index_files/close-icon.png" alt="" /></a>
+      </div>
     </div>
   );
 };
