23583ae647865c4a0693729f13c43631f1087d12
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-12-03 23:09:15 +0200

    added top section data block.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -84,10 +84,99 @@ const HomePage: React.FC = () => {
                 </ul>
               </div>
             </div>
+
+            <div className="main-card-content">
+              <div className="overview-col">
+                <div className="overview-inner">
+                  <img src="/of-icon.svg" alt="" />
+                  <p>Total earnings</p>
+                  <h5 className="total-earning" id="editableText">
+                    <span
+                      className="dollar-sign"
+                      style={{
+                        fontSize: '24px',
+                        color: '#2D74FF',
+                        lineHeight: '48px',
+                        verticalAlign: 'middle',
+                      }}
+                    >
+                      $
+                    </span>
+                    7.00
+                  </h5>
+                </div>
+              </div>
+
+              <div className="overview-col">
+                <div className="overview-card">
+                  <div className="card-content">
+                    <h5 className="sub-earning" id="editableText">
+                      $2.20
+                    </h5>
+                    <p>Subscriptions</p>
+                  </div>
+                  <img src="/plus-icon.png" alt="" />
+                </div>
+
+                <div className="overview-card">
+                  <div className="card-content">
+                    <h5 className="tips-earning" id="editableText">
+                      $0.00
+                    </h5>
+                    <p>Tips</p>
+                  </div>
+                  <img src="/tips-icon.png" alt="" />
+                </div>
+              </div>
+
+              <div className="overview-col">
+                <div className="overview-card">
+                  <div className="card-content">
+                    <h5 className="posts-earning" id="editableText">
+                      $0.00
+                    </h5>
+                    <p>Posts</p>
+                  </div>
+                  <img src="/posts-icon.png" alt="" />
+                </div>
+
+                <div className="overview-card">
+                  <div className="card-content">
+                    <h5 className="ref-earning" id="editableText">
+                      $0.00
+                    </h5>
+                    <p>Referrals</p>
+                  </div>
+                  <img src="/referrals-icon.png" alt="" />
+                </div>
+              </div>
+
+              <div className="overview-col">
+                <div className="overview-card">
+                  <div className="card-content">
+                    <h5 className="msg-earning" id="editableText">
+                      $4.80
+                    </h5>
+                    <p>Messages</p>
+                  </div>
+                  <img src="/messages-icon.png" alt="" />
+                </div>
+
+                <div className="overview-card">
+                  <div className="card-content">
+                    <h5 className="streams-earning" id="editableText">
+                      $0.00
+                    </h5>
+                    <p>Streams</p>
+                  </div>
+                  <img src="/streams-icon.png" alt="" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      Home page
+
     </div>
   );
 };
