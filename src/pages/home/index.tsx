596814d901d4569a90b15a27be9bd6e99c112a44
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-12-03 23:36:59 +0200

    almost finished UI/UX work for home page.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -190,9 +190,9 @@ const HomePage: React.FC = () => {
               <p>No data</p>
             </div>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1  }}>
+          <div style={{display: 'flex', flexDirection: 'column', flexGrow: 1}}>
             <div style={{display: 'flex', flexDirection: 'column', flexGrow: 1, backgroundColor: 'transparent'}}>
-              <div className="main-card-wrap charts1">
+              <div className="main-card-wrap charts1" style={{ marginTop: 0 }}>
                 <div className="main-card-heading">
                   <h4>Current clocked-in employees
                     <button
@@ -208,10 +208,27 @@ const HomePage: React.FC = () => {
                     </button>
                   </h4>
                 </div>
-                <div style={{position: 'relative', top:'0%', padding: 0}} className="main-card-content nodata">
+                <div style={{position: 'relative', top: '0%', padding: 0}} className="main-card-content nodata">
                   <img src="/empty-data.png" alt=""/>
                   <p>No employees have clocked in.</p>
                 </div>
+              </div>
+              <div id="employee-sales" data-highcharts-chart="0">
+                {/* Highchart goes here */}
+              </div>
+            </div>
+            <div className="main-card-wrap charts1">
+              <div className="main-card-heading">
+                <h4>Scheduled hours
+                  <button type="button" className="tooltip-custom" data-bs-toggle="tooltip" data-bs-placement="top"
+                          data-bs-title="Tooltip on top">
+                    <img src="/info-icon.png" alt=""/>
+                  </button>
+                </h4>
+              </div>
+              <div className="main-card-content nodata">
+                <img src="/empty-data.png" alt=""/>
+                <p>No data</p>
               </div>
             </div>
           </div>
